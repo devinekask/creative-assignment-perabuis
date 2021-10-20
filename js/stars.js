92b2e29601b874add0ab580f78482b7502fc068e
Silke Janssens <silke.janssens@student.howest.be>
2021-10-20 12:36:37 +0200

    star animation complete

--- js/stars.js
+++ js/stars.js
@@ -1,11 +1,32 @@
-//a very special js file to animate the background stars
+const getRandomNumber = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is exclusive and the minimum is inclusive
+}
 
-console.log(window.innerWidth);
-console.log(window.innerHeight);
+
+// ANIMATE THE STARS IN THE BACKGROUND
+const maxX = window.innerWidth;
+const maxY = window.innerHeight
+const maxAnimation = 4;
+const maxImages = 8;
+const numberOfStars = 100;
+
+const $starsHtml = document.querySelector('.stars');
 
 let stars = [];
 
 const createStars = () => {
+    for (let i = 0; i < numberOfStars; i++) {
+        const size = getRandomNumber(5, 15);
+      stars.push(`<img class="starAnimation${getRandomNumber(1, maxAnimation)}" src="assets/img/background/star${getRandomNumber(1, maxImages)}.png" style="left:${getRandomNumber(0, maxX)}px; top: ${getRandomNumber(0, maxY)}px;" alt="background" width="${size}" height="${size}">`)
+    }
+    stars.forEach(star => {
+        $starsHtml.innerHTML += star;
+    })
+
 
 };
+
 createStars();
+
